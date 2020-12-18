6e959d93ca3b3b661111dfd632aa851d20f5a3df
Arman Yaraee <arman@yaraee.net>
2020-12-18 16:39:56 +0000

    Added a size only test to cover all cases

--- ConstructionLine.CodingChallenge.Tests/SearchEnginePerformanceTests.cs
+++ ConstructionLine.CodingChallenge.Tests/SearchEnginePerformanceTests.cs
@@ -46,6 +46,27 @@ namespace ConstructionLine.CodingChallenge.Tests
         }
 
         [Test]
+        public void PerformanceTest_SizeOnly()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var options = new SearchOptions
+            {
+                Sizes = new List<Size> { Size.Small, Size.Medium }
+            };
+
+            var results = _searchEngine.Search(options);
+
+            sw.Stop();
+            Console.WriteLine($"Test fixture finished in {sw.ElapsedMilliseconds} milliseconds");
+
+            AssertResults(results.Shirts, options);
+            AssertSizeCounts(_shirts, options, results.SizeCounts);
+            AssertColorCounts(_shirts, options, results.ColorCounts);
+        }
+
+        [Test]
         public void PerformanceTest_SizeAndColor()
         {
             var sw = new Stopwatch();
